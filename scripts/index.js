21d6cc77d4271e009d3a3c986481c706a633c0a4
Ainur Valiullin <tesla77750@yandex.ru>
2021-03-27 01:38:40 +0500

    final 1.0

---
+++ scripts/index.js
@@ -0,0 +1,30 @@
+let editButton = document.querySelector('.profile__edit_button');
+let popup = document.querySelector('.popup');
+let closeButton = document.querySelector('.popup__close-button');
+let profileUserName = document.querySelector('.profile__user_name');
+let profileUserInfo = document.querySelector('.profile__user_info');
+let popupUserName = document.querySelector('.popup__user-name');
+let popupUserInfo = document.querySelector('.popup__user-info');
+let popupSubmit = document.querySelector('form');
+
+function popupOpened () {
+    popup.classList.add('popup_opened');
+    popupUserName.setAttribute('value', profileUserName.textContent);
+    popupUserInfo.setAttribute('value', profileUserInfo.textContent);
+}
+
+function popupClosed () {
+    popup.classList.remove('popup_opened');
+}
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    profileUserName.textContent = popupUserName.value;
+    profileUserInfo.textContent = popupUserInfo.value;
+    popupClosed();
+}
+
+
+editButton.addEventListener('click', popupOpened);
+closeButton.addEventListener('click', popupClosed);
+popupSubmit.addEventListener('submit', formSubmitHandler);
